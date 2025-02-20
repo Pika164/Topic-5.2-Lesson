ff21511aea3c0ba1dbac2f1ebc639c5762e69756
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-20 14:44:52 -0500

    hasdfa

--- Topic 5.2 Lesson/Program.cs
+++ Topic 5.2 Lesson/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string choice;
+
+            Console.WriteLine("Welcome to this short quiz:");
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to Continue");
+            Console.WriteLine();
+            Console.WriteLine("Question 1: Can a computer crash?");
+            Console.WriteLine();
+            Console.WriteLine("A: I dont know         " + "B: Yes");
+            Console.WriteLine("C: I will have to find out   " + "D: No");
+            if (choice == "A")
+            {
+                Console.WriteLine("I dont know doesnt cut it bud");
+            }
+            else if (choice == "B")
+            {
+                Console.WriteLine("You are correct!");
+            }
         }
     }
 }
